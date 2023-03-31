fb8ffcb446219dd757cc166ea6e88933edd8402c
ErnestRamazani <ernestrams4124@gmail.com>
2023-03-30 23:40:36 -0400

    Update main.cpp

--- project 3/main.cpp
+++ project 3/main.cpp
@@ -9,11 +9,15 @@
 #include <random>
 #include "graph.h"
 
+//Represents a message in the system, containing the number of hops 
+//it has taken and its travel time.
 struct Message {
     int hops;
     double travel_time;
 };
 
+//Contains statistics for each working thread, including the number of messages received and forwarded, 
+//the total hops, and the total travel time.
 struct ThreadStatistics {
     int messages_received;
     int messages_forwarded;
@@ -22,15 +26,19 @@ struct ThreadStatistics {
     std::mutex total_travel_time_mutex;
 };
 
+//A thread - safe message queue that allows sending and receiving messages.
 class MessageQueue {
 public:
+
+    //Sends a message to the message queue in a thread-safe manner, using a mutex and condition variable.
     void send(const Message& msg) {
         std::unique_lock<std::mutex> lock(mutex_);
         queue_.push(msg);
         lock.unlock();
         cond_var_.notify_one();
     }
-
+    
+    //Tries to receive a message from the message queue. Returns true if a message is received, false otherwise.
     bool try_receive(Message& msg) {
         std::unique_lock<std::mutex> lock(mutex_);
         if (!queue_.empty()) {
@@ -47,12 +55,15 @@ private:
     std::condition_variable cond_var_;
 };
 
+//Returns a random double value within the specified range.
 double rand_range(double min, double max) {
     static std::random_device rd;
     static std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dist(min, max);
     return dist(gen);
 }
+
+//The main function executed by each thread. Processes messages and forwards them to random neighbors in the graph.
 
 void workingThread(ThreadStatistics& stats, MessageQueue& mq, std::atomic<bool>& terminate, const Graph& graph, int nodeId, std::vector<MessageQueue>& all_queues) {
     const auto& neighbors = graph.getNeighbors(nodeId);
@@ -61,17 +72,18 @@ void workingThread(ThreadStatistics& stats, MessageQueue& mq, std::atomic<bool>&
         Message received_message;
         if (mq.try_receive(received_message)) {
             
+            // Process the received message
             stats.messages_received++;
             received_message.hops++;
             received_message.travel_time += rand_range(0.01, 0.1);
 
-            
+            // Update the total_travel_time safely
             {
                 std::unique_lock<std::mutex> lock(stats.total_travel_time_mutex);
                 stats.total_travel_time += received_message.travel_time;
             }
 
-          //Forward message if necessary 
+            //Forward message if necessary 
             if (received_message.hops < 20) {
                 std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(rand_range(100, 500)));
 
@@ -92,6 +104,10 @@ void workingThread(ThreadStatistics& stats, MessageQueue& mq, std::atomic<bool>&
         }
     }
 }
+
+//The main function of the program.Initializes the graph, threads, 
+//message queues, and statistics.Sends initial messages, waits for a 
+//specified time, then terminates the threadsand prints the statistics.
 int main() {
     Graph graph("a28.dat"); 
 
